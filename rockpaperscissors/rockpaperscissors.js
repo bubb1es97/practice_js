2563175cb4a403257e286bc78e7758891ef37385
bubb1es97 <lizzyjane97@yahoo.com>
2024-04-06 17:04:08 -0400

    create function that randomly returns 'Rock', 'Paper' or 'Scissors'.

--- rockpaperscissors/rockpaperscissors.js
+++ rockpaperscissors/rockpaperscissors.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+// create a function that tells the computer to select rock, paper or scissors randomly.
+function getComputerChoice(){
+   let arr = ['rock', 'paper', 'scissors']; // place random items in one variable via an array
+   return (arr[(Math.floor(Math.random() * arr.length))]); // tell computer to access variable and pick random string. 
+}
+
+console.log(getComputerChoice());
